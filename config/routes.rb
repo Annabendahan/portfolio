849f5b7f2414df416c1b95ad748bc1a840727647
Anna Bendahan <an.bendahan@gmail.com>
2018-09-05 18:46:00 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+  get 'download_pdf', to: "pages#download_pdf"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
